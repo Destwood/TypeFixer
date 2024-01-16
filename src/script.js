271b7c4d00c8d91d6c6f6c493929cc4368d2121d
Destwood <DestwoodGames@Gmail.com>
2024-01-16 11:38:49 +0200

    16.01

--- src/script.js
+++ src/script.js
@@ -1,347 +1,240 @@
+const cursorDiv = document.createElement("div");
+cursorDiv.className = "Existek";
 const dictionary = {
   cat: ["dog", "rat", "bat"],
   helo: ["hello", "help", "hell"],
   heldp: ["help", "held", "hello"],
 };
+
+let isMenuActive = false;
 let currentWord = "";
 let currentWordBeforeCursor = false;
-let isMenuActive = false;
-const cursorDiv = document.createElement("div");
-function getCaretCoordinates(inputElement, caretPos) {
-  const rect = inputElement.getBoundingClientRect();
-  const div = document.createElement("div");
-  const inputStyle = window.getComputedStyle(inputElement);
+let activeElementInfo = {
+  element: null,
+  isInputOrTextarea: false,
+  isContenteditable: false,
+};
 
-  div.textContent = inputElement.value.substring(0, caretPos);
-  div.style.whiteSpace = "pre-wrap";
-  div.style.visibility = "hidden";
-  div.style.position = "absolute";
-  div.style.top = "0";
-  div.style.left = "0";
-  div.style.width = "auto";
-  div.style.height = "auto";
-  div.style.fontSize = inputStyle.fontSize;
-  div.style.fontFamily = inputStyle.fontFamily;
-  document.body.appendChild(div);
-
-  const spanRect = div.getBoundingClientRect();
-  let x = rect.left + spanRect.width;
-  let y = rect.top + spanRect.height;
-
-  if (y > 30) {
-    y -= 55;
-  } else {
-    y = 30;
-  }
-  if (x > 60) {
-    x -= 50;
-  } else {
-    x = 5;
-  }
-
-  document.body.removeChild(div);
-
-  cursorDiv.style.top = y + "px";
-  cursorDiv.style.left = x + "px";
-}
-function getCaretContentEditable(rect) {
-  let x = rect.x;
-  let y = rect.y;
-
-  if (y > 50) {
-    y -= 35;
-  } else {
-    y = 30;
-  }
-  if (x > 75) {
-    x -= 70;
-  } else {
-    x = 5;
-  }
-  cursorDiv.style.top = y + "px";
-  cursorDiv.style.left = x + "px";
-}
-
-function getWordBeforeCursor() {
-  currentWordBeforeCursor = true;
-  const activeElement = document.activeElement;
+function getCurrentWord() {
+  // return text if selected
+  if (window.getSelection().toString()) return window.getSelection().toString()
+  const activeElement = activeElementInfo.element;
   if (!activeElement) return "";
-  const isInputText =
-    activeElement.tagName === "INPUT" && activeElement.type === "text";
-  const isTextarea = activeElement.tagName === "TEXTAREA";
-  const isContenteditable =
-    activeElement.getAttribute("contenteditable") === "true";
 
-  if (isInputText || isTextarea) {
-    const cursorPosition = activeElement.selectionStart;
-    const text = activeElement.value || activeElement.textContent;
-    const words = text.substring(0, cursorPosition).trim().split(/\s+/);
-
-    getCaretCoordinates(activeElement, cursorPosition);
-    return words[words.length - 1];
-  } else if (isContenteditable) {
+  let text, textBeforeCursor, textAfterCursor, cursorPosition, rect;
+  if (activeElementInfo.isInputOrTextarea) {
+    cursorPosition = activeElement.selectionStart;
+    text = activeElement.value || activeElement.textContent;
+    rect = activeElement.getBoundingClientRect();
+    if(currentWordBeforeCursor) {
+      const words = text.substring(0, cursorPosition).trim().split(/\s+/);
+      getCoordinates(rect, cursorPosition);
+      return words[words.length - 1];
+    }
+    textBeforeCursor = text.substring(0, cursorPosition);
+    textAfterCursor = text.substring(cursorPosition);
+  } else if (activeElementInfo.isContenteditable) {
     const selection = window.getSelection();
     const range = selection.getRangeAt(0);
-    const rect = range.getBoundingClientRect();
-    getCaretContentEditable(rect);
-
-    const wordsBeforeCursor = range.startContainer.textContent
-      .substring(0, range.startOffset)
-      .trim()
-      .split(/\s+/);
-    return wordsBeforeCursor[wordsBeforeCursor.length - 1];
-  }
-  return "";
-}
-function getWordUnderCursor() {
-  currentWordBeforeCursor = false;
-  const activeElement = document.activeElement;
-  if (window.getSelection().toString()) {
-    return window.getSelection().toString();
-  }
-  if (!activeElement) return "";
-  const isInputOrTextarea =
-    activeElement.tagName === "INPUT" || activeElement.tagName === "TEXTAREA";
-  const isContenteditable =
-    activeElement.getAttribute("contenteditable") === "true";
-  if (isInputOrTextarea) {
-    const cursorPosition = activeElement.selectionStart;
-    const inputString = activeElement.value || activeElement.textContent;
-    getCaretCoordinates(activeElement, cursorPosition);
-    const textBeforeCursor = inputString.substring(0, cursorPosition);
-    const textAfterCursor = inputString.substring(cursorPosition);
-    const spaceBeforeCursor = textBeforeCursor.endsWith(" ");
-    const spaceAfterCursor = textAfterCursor.startsWith(" ");
-    const wordsBeforeCursor = textBeforeCursor.split(" ");
-    const wordsAfterCursor = textAfterCursor.split(" ");
-    if (spaceBeforeCursor) {
-      return wordsAfterCursor[0];
-    } else if (spaceAfterCursor) {
-      return (
-        wordsBeforeCursor[wordsBeforeCursor.length - 1] + wordsAfterCursor[0]
-      );
-    } else {
-      return (
-        wordsBeforeCursor[wordsBeforeCursor.length - 1] + wordsAfterCursor[0]
-      );
-    }
-  } else if (isContenteditable) {
-    const selection = window.getSelection();
-    const range = selection.getRangeAt(0);
-    const rect = range.getBoundingClientRect();
-    getCaretContentEditable(rect);
-    const selectionStart = range.startOffset;
-    const selectionEnd = range.endOffset;
-    const textBeforeCursor = range.startContainer.textContent.substring(
-      0,
-      selectionStart
-    );
-    const textAfterCursor =
-      range.startContainer.textContent.substring(selectionStart);
-
-    const beforeCursorHasSpace = /\s$/.test(textBeforeCursor);
-    const afterCursorHasSpace = /^\s/.test(textAfterCursor);
-
-    const wordsBeforeCursor = textBeforeCursor.trim().split(/\s+/);
-    const wordsAfterCursor = textAfterCursor.trim().split(/\s+/);
-    const beforeAfterSpaces =
-      textBeforeCursor.slice(-1) + textAfterCursor.slice(0, 1);
-    // const selectedWordStart =
-    //   selectionStart - wordsBeforeCursor[wordsBeforeCursor.length - 1].length;
-    // const selectedWordEnd = selectionStart + wordsAfterCursor[0].length - 1;
-
-    // console.log("Selected Word Start:", selectedWordStart);
-    // console.log("Selected Word End:", selectedWordEnd);
-    if (beforeAfterSpaces === "  ") {
-      return "";
-    }
-
-    if (beforeCursorHasSpace) {
-      return wordsAfterCursor[0].toLowerCase();
-    } else if (afterCursorHasSpace) {
+    rect = range.getBoundingClientRect();
+    if(currentWordBeforeCursor) {
+      getCoordinates(rect, cursorPosition);
+      const wordsBeforeCursor = range.startContainer.textContent
+        .substring(0, range.startOffset)
+        .trim()
+        .split(/\s+/);
       return wordsBeforeCursor[wordsBeforeCursor.length - 1];
-    } else {
-      return (
-        wordsBeforeCursor[wordsBeforeCursor.length - 1] +
-        wordsAfterCursor[0].toLowerCase()
-      );
     }
+    text = range.startOffset;
+    textBeforeCursor = range.startContainer.textContent.substring(0, text );
+    textAfterCursor = range.startContainer.textContent.substring(text);
   }
-  return "";
+  getCoordinates(rect, cursorPosition);
+  const spaceBeforeCursor = /\s$/.test(textBeforeCursor);
+  const spaceAfterCursor = /^\s/.test(textAfterCursor);
+  const wordsBeforeCursor = textBeforeCursor.trim().split(/\s+/);
+  const wordsAfterCursor = textAfterCursor.trim().split(/\s+/);
+
+  if (spaceBeforeCursor) {
+    return spaceAfterCursor ? "" : wordsAfterCursor[0];
+  } else if (spaceAfterCursor) {
+    return wordsBeforeCursor[wordsBeforeCursor.length - 1];
+  } else {
+    return (
+      wordsBeforeCursor[wordsBeforeCursor.length - 1] + wordsAfterCursor[0]
+    );
+  }
 }
+function getCoordinates(rect, cursorPosition) {
+  let x, y;
+  
+  if (activeElementInfo.isContenteditable) {
+    x = rect.x;
+    y = rect.y;
+  } else {
+    const activeElement = activeElementInfo.element;
+    const div = document.createElement("div");
+    const inputStyle = window.getComputedStyle(activeElement);
+
+    div.textContent = activeElement.value.substring(0, cursorPosition);
+    div.style.whiteSpace = "pre-wrap";
+    div.style.visibility = "hidden";
+    div.style.position = "absolute";
+    div.style.top = "0";
+    div.style.left = "0";
+    div.style.width = "auto";
+    div.style.height = "auto";
+    div.style.fontSize = inputStyle.fontSize;
+    div.style.fontFamily = inputStyle.fontFamily;
+    document.body.appendChild(div);
+    const divRect = div.getBoundingClientRect();
+    document.body.removeChild(div);
+
+    x = rect.left + divRect.width;
+    y = rect.top + divRect.height;
+  }
+  y = y > 50 ? y - 45 : 30;
+  x = x > 75 ? x - 70 : 5;
+  cursorDiv.style.top = y + "px";
+  cursorDiv.style.left = x + "px";
+}
+
+function replaceWord(item) {
+  let textBeforeCursor, textAfterCursor, range, selection, cursorPosition;
+  const activeElement = activeElementInfo.element;
+  const isContenteditable = activeElementInfo.isContenteditable;
+
+  if (isContenteditable) {
+    selection = window.getSelection();
+    range = selection.getRangeAt(0);
+    cursorPosition = range.startOffset;
+    textBeforeCursor = range.startContainer.textContent.substring(0, cursorPosition);
+    textAfterCursor = range.startContainer.textContent.substring(cursorPosition);
+  } else {
+    cursorPosition = activeElement.selectionStart;
+    const inputString = activeElement.value;
+    textBeforeCursor = inputString.substring(0, cursorPosition);
+    textAfterCursor = inputString.substring(cursorPosition);
+  }
+
+  const spaceBeforeCursor = textBeforeCursor.endsWith(" ");
+  const spaceAfterCursor = textAfterCursor.startsWith(" ");
+  const wordBeforeCursorDeleted = textBeforeCursor.replace(/\S+\s*$/, "");
+  const wordAfterCursorDeleted = textAfterCursor.replace(/^\s*\S+\s*/, "");
+  let newText = "";
+  let newCursorPosition = 0;
+  
+  newText = spaceBeforeCursor
+  ? currentWordBeforeCursor
+    ? wordBeforeCursorDeleted + `${item} ` + textAfterCursor
+    : textBeforeCursor + `${item} ` + wordAfterCursorDeleted
+  : spaceAfterCursor
+  ? wordBeforeCursorDeleted + `${item}` + textAfterCursor
+  : wordBeforeCursorDeleted + `${item} ` + wordAfterCursorDeleted;
+
+  newCursorPosition = newText.indexOf(item) + item.length;
+
+  if (isContenteditable) {
+    range.startContainer.textContent = newText;
+    range.setStart(range.startContainer, newCursorPosition);
+    range.collapse(true);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  } else {
+    activeElement.value = newText;
+    activeElement.focus();
+    activeElement.setSelectionRange(newCursorPosition, newCursorPosition);
+  }
+  closeMenu();
+}
+function updateActiveElementInfo() {
+  const activeElement = document.activeElement;
+  activeElementInfo.element = activeElement;
+  activeElementInfo.isInputOrTextarea =
+    activeElement.tagName === "INPUT" || activeElement.tagName === "TEXTAREA";
+  activeElementInfo.isContenteditable =
+    activeElement.getAttribute("contenteditable") === "true";
+}
+
 const openMenu = () => {
+  isMenuActive = true;
+
+  const wordsList = dictionary[currentWord.toLocaleLowerCase()];
   cursorDiv.innerHTML = "";
 
-  isMenuActive = true;
-  const activeElement = document.activeElement;
-  const isInputOrTextarea =
-    activeElement.tagName === "INPUT" || activeElement.tagName === "TEXTAREA";
-  const isContenteditable =
-    activeElement.getAttribute("contenteditable") === "true";
-
-  cursorDiv.className = "Existek";
-  const settings = document.createElement("input");
-  settings.className = "settings";
-  settings.type = "color";
-  const wordsList = dictionary[currentWord.toLocaleLowerCase()];
   wordsList.forEach((item) => {
     const option = document.createElement("button");
     option.className = "optionButton";
     option.innerText = item;
     option.addEventListener("click", () => {
-      if (
-        currentWord !== null &&
-        dictionary.hasOwnProperty(currentWord.toLocaleLowerCase())
-      ) {
-        if (isInputOrTextarea) {
-          replaceInputOrTextarea(item, activeElement);
-        } else if (isContenteditable) {
-          replaceContentEditable(item);
-        }
+      if (dictionary.hasOwnProperty(currentWord.toLocaleLowerCase())) {
+        replaceWord(item);
       }
     });
     cursorDiv.appendChild(option);
   });
-  if (localStorage.getItem("selectedColor")) {
-    cursorDiv.style.backgroundColor = localStorage.getItem("selectedColor");
+
+  // Background color feature
+  let settings = cursorDiv.querySelector(".settings");
+  if (!settings) {
+    settings = document.createElement("input");
+    settings.className = "settings";
+    settings.type = "color";
+    cursorDiv.appendChild(settings);  if (localStorage.getItem("selectedColor")) {
+      cursorDiv.style.backgroundColor = localStorage.getItem("selectedColor");
+    }
+    settings.addEventListener("input", (event) => {
+      const selectedColor = event.target.value;
+      localStorage.setItem("selectedColor", selectedColor);
+      cursorDiv.style.backgroundColor = selectedColor;
+    });
   }
 
-  settings.addEventListener("input", (event) => {
-    const selectedColor = event.target.value;
-    localStorage.setItem("selectedColor", selectedColor);
-    cursorDiv.style.backgroundColor = selectedColor;
-  });
-
-  cursorDiv.appendChild(settings);
   document.body.appendChild(cursorDiv);
 };
+
 const closeMenu = () => {
   isMenuActive = false;
-  cursorDiv.innerHTML = "";
   cursorDiv.remove();
 };
-
-function replaceInputOrTextarea(item, activeElement) {
-  const cursorPosition = activeElement.selectionStart;
-  const inputString = activeElement.value;
-  const textBeforeCursor = inputString.substring(0, cursorPosition);
-  const textAfterCursor = inputString.substring(cursorPosition);
-  const spaceBeforeCursor = textBeforeCursor.endsWith(" ");
-  const spaceAfterCursor = textAfterCursor.startsWith(" ");
-  const wordBeforeCursorDeleted = textBeforeCursor.replace(/\S+\s*$/, "");
-  const wordAfterCursorDeleted = textAfterCursor.replace(/^\s*\S+\s*/, "");
-  let newText = "";
-  let newCursorPosition = 0;
-  if (spaceBeforeCursor) {
-    if (currentWordBeforeCursor) {
-      newText = wordBeforeCursorDeleted + `${item} ` + textAfterCursor;
-      newCursorPosition = wordBeforeCursorDeleted.length + item.length;
-    } else {
-      newText = textBeforeCursor + `${item} ` + wordAfterCursorDeleted;
-      newCursorPosition = textBeforeCursor.length + item.length;
-    }
-  } else if (spaceAfterCursor) {
-    newText = wordBeforeCursorDeleted + `${item}` + textAfterCursor;
-    newCursorPosition = wordBeforeCursorDeleted.length + item.length;
-  } else {
-    newText = wordBeforeCursorDeleted + `${item} ` + wordAfterCursorDeleted;
-    newCursorPosition = wordBeforeCursorDeleted.length + item.length;
-  }
-
-  activeElement.value = newText;
-  activeElement.focus();
-  activeElement.setSelectionRange(newCursorPosition, newCursorPosition);
-
-  closeMenu();
-}
-
-function replaceContentEditable(item) {
-  const selection = window.getSelection();
-  const range = selection.getRangeAt(0);
-  const cursorPosition = range.startOffset;
-  const textBeforeCursor = range.startContainer.textContent.substring(
-    0,
-    cursorPosition
-  );
-  const textAfterCursor =
-    range.startContainer.textContent.substring(cursorPosition);
-  const spaceBeforeCursor = textBeforeCursor.endsWith(" ");
-  const spaceAfterCursor = textAfterCursor.startsWith(" ");
-  const wordBeforeCursorDeleted = textBeforeCursor.replace(/\S+\s*$/, "");
-  const wordAfterCursorDeleted = textAfterCursor.replace(/^\s*\S+\s*/, "");
-  let newText = "";
-  let newCursorPosition = 0;
-
-  if (spaceBeforeCursor) {
-    if (currentWordBeforeCursor) {
-      newText = wordBeforeCursorDeleted + `${item} ` + textAfterCursor;
-      newCursorPosition = wordBeforeCursorDeleted.length + item.length;
-    } else {
-      newText = textBeforeCursor + `${item} ` + wordAfterCursorDeleted;
-      newCursorPosition = textBeforeCursor.length + item.length;
-    }
-  } else if (spaceAfterCursor) {
-    newText = wordBeforeCursorDeleted + `${item}` + textAfterCursor;
-    newCursorPosition = wordBeforeCursorDeleted.length + item.length;
-  } else {
-    newText = wordBeforeCursorDeleted + `${item} ` + wordAfterCursorDeleted;
-    newCursorPosition = wordBeforeCursorDeleted.length + item.length;
-  }
-
-  range.startContainer.textContent = newText;
-
-  range.setStart(range.startContainer, newCursorPosition);
-  range.collapse(true);
-
-  selection.removeAllRanges();
-  selection.addRange(range);
-  closeMenu();
-}
-
 function handleEvent() {
   if (
-    currentWord !== null &&
-    dictionary.hasOwnProperty(currentWord.toLocaleLowerCase())
+    dictionary.hasOwnProperty(currentWord.toLocaleLowerCase()) &&
+    (activeElementInfo.isContenteditable || activeElementInfo.isInputOrTextarea)
   ) {
     openMenu();
-  } else if (isMenuActive) {
-    closeMenu();
+    return;
   }
+  closeMenu();
 }
-
 const bodyEvents = () => {
-  const elements = document.querySelectorAll(
-    'input[type="text"], textarea, [contenteditable="true"]'
-  );
-
-  elements.forEach((element) => {
-    element.addEventListener("input", (event) => {
-      if (event.data === " ") {
-        currentWord = getWordUnderCursor();
-      }
-    });
-  });
-
+  // handling click event
   document.addEventListener("click", (e) => {
-    // console.log("click\n\n\n\n\n");
     if (!cursorDiv.contains(e.target)) {
-      currentWord = getWordUnderCursor();
-      handleEvent();
+      updateActiveElementInfo();
+      if (activeElementInfo.isContenteditable || activeElementInfo.isInputOrTextarea) {
+        currentWordBeforeCursor = false;
+        currentWord = getCurrentWord();
+        handleEvent();
+      }
     }
   });
 
+  // handle Arrow or Space button
   let lastKeyPressed = "";
   document.addEventListener("keyup", (event) => {
-    // console.log("button pressed\n\n\n\n\n");
     if (event.code === "Space") {
-      if (lastKeyPressed === "Space") {
+      currentWordBeforeCursor = true;
+      // if menu is open => closing it if space pressed twice
+      if (lastKeyPressed === "Space" || isMenuActive) {
         closeMenu();
       } else {
-        currentWord = getWordBeforeCursor();
+        currentWord = getCurrentWord();
         handleEvent();
       }
     } else if (event.code.includes("Arrow")) {
-      currentWord = getWordUnderCursor();
+      currentWordBeforeCursor = false;
+      currentWord = getCurrentWord();
       handleEvent();
     } else if (isMenuActive) {
       closeMenu();
